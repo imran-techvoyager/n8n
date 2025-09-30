85f42625594a81b4b9d9eb97b8e8f92f2a5f6f34
imran-techvoyager <imran.techvoyager@gmail.com>
2025-09-30 23:32:22 +0530

    remove dubuggin logs

--- apps/execution-engine/src/lib/redis.ts
+++ apps/execution-engine/src/lib/redis.ts
@@ -2,7 +2,6 @@ import { createClient } from "redis";
 
 const createRedisClient = async () => {
   const URL = process.env.REDIS_URL || "redis://localhost:6379";
-  console.log("URL", URL);
   const client = createClient({
     url: URL,
   });
